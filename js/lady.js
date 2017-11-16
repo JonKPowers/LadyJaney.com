86e3798fac93ae3da251b09ad4584e93454e710c
Jon Powers <Jon.K.Powers+git@gmail.com>
2017-11-15 20:48:08 -0500

    Final project changes

--- js/lady.js
+++ js/lady.js
@@ -4,20 +4,30 @@
 //Photo Carousel
 ///////////////////////////////////////////////
 
-const images = ["img/1.JPG",
-                "img/2.JPG",
-                "img/3.JPG",
-                //"img/4.JPG",
-                "img/5.JPG",
-                "img/6.JPG",
-                "img/7.JPG",
-                "img/8.JPG",
-                "img/9.JPG",
-                "img/10.JPG",
-                "img/11.JPG",
-                "img/12.JPG",
-                "img/13.JPG",
-                "img/14.JPG"
+const images = ["img/0",
+                //"img/1",
+                "img/2",
+                "img/3",
+                //"img/4",
+                //"img/5",
+                "img/6",
+                //"img/7",
+                "img/8",
+                "img/9",
+                "img/10",
+                "img/11",
+                "img/12",
+                "img/13",
+                "img/14",
+                "img/15",
+                "img/16",
+                "img/17",
+                "img/18",
+                "img/19",
+                "img/20",
+                "img/21",
+                "img/22",
+                "img/23"
 ]
 
 let firstDisplayedImage = 0;
@@ -26,6 +36,7 @@ const smallThumbs = document.querySelector('#thumbs_small')
 const medThumbs = document.querySelector('#thumbs_med')
 
 const circulatePhotoIndex = (imageIndex) => {
+    //This function keeps the image index rotating after reaching the first or last items in the image array
     if (imageIndex < 0) {
         imageIndex = images.length + imageIndex;
     } else if (imageIndex >= images.length) {
@@ -45,58 +56,60 @@ const advancePhotos = (direction, num) => {
     //Large photo div
     let htmlCode = '';
     for (let i = 0; i < 3; i++) {
-        htmlCode += `<img class="lightitup" src="${images[circulatePhotoIndex(firstDisplayedImage + i)]}" alt="">`;
+        htmlCode += `<img class="lightitup" src="${images[circulatePhotoIndex(firstDisplayedImage + i)]}_small.jpg" target="${images[circulatePhotoIndex(firstDisplayedImage + i)]}.jpg" alt="">`;
     }
     thumbsLg.innerHTML = htmlCode;
 
     //Med photo div
     htmlCode = '';
     for (let i = 0; i < 2; i++) {
-        htmlCode += `<img class="lightitup" src="${images[circulatePhotoIndex(firstDisplayedImage + i)]}" alt="">`;
+        htmlCode += `<img class="lightitup" src="${images[circulatePhotoIndex(firstDisplayedImage + i)]}_small.jpg" target="${images[circulatePhotoIndex(firstDisplayedImage + i)]}.jpg" alt="">`;
     }
     medThumbs.innerHTML = htmlCode;
 
     //Small photo div
-    smallThumbs.innerHTML = `<img class="lightitup" src="${images[circulatePhotoIndex(firstDisplayedImage)]}" alt="">`;
+    smallThumbs.innerHTML = `<img class="lightitup" src="${images[circulatePhotoIndex(firstDisplayedImage)]}_small.jpg" target="${images[circulatePhotoIndex(firstDisplayedImage)]}.jpg" alt="">`;
+
+    //Rebuild the lightbox for newly added items
+    add_lightbox();
 }
 
-advancePhotos();
-$('body').scrollspy({ target: '#main-nav', offset:200 });
+/////////////////////////////////////////////
+//Lightbox--This code will build a lightbox with the currently displayed images
+/////////////////////////////////////////////
+//Lightbox Gallary feature
 
-///////////////////////////////////////////////
-//lightbox
-///////////////////////////////////////////////
-$(document).ready(function() {
-    //Lightbox Gallary feature
+const add_lightbox = () => {
     var $lightbox = $("<div class='lightbox'></div>");
     console.log($lightbox);
     var $img = $("<img>");
 
-    // Hide the lightbox
+
     $lightbox.hide();
 
-    // Add image and caption to lightbox
+    // Build up the lightbox
     $lightbox.append($img)
-
-    // Add lighbox to document
     $('body').append($lightbox);
 
-    //Listener for gallery images
+    //Add listener
     $('.lightitup').click(function(e) {
       e.preventDefault();
-
-    // Get image link and description
-      var src = $(this).attr("src");
-
-    // Add data to lighbox
+      var src = $(this).attr("target");
       $img.attr('src', src);
-
-    // Show lightbox
       $lightbox.fadeIn('fast');
 
-    // Exit the lightbox when the div is clicked anywhere
+    // Exit lightbox on a click
       $lightbox.click(function() {
         $lightbox.fadeOut('fast');
       });
     });
+}
+
+/////////////////////////////////////////////
+//Fire up the page!
+/////////////////////////////////////////////
+
+$(document).ready(function() {
+    advancePhotos();
+    $('body').scrollspy({ target: '#main-nav', offset:200 });
 });
